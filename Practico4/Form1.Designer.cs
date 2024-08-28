c5e68be48ba4e7e0fa7adef11a6d02053ee66f5a
Pietro <pietrocalathaki@gmail.com>
2024-08-28 17:23:29 -0300

    Agregar archivos de proyecto.

---
+++ Practico4/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace Practico4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.FunImp = new System.Windows.Forms.Button();
+            this.FunPar = new System.Windows.Forms.Button();
+            this.FunPrim = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desde";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasta";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generar Funcion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lista de Numeros";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(188, 76);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(236, 253);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // FunImp
+            // 
+            this.FunImp.Location = new System.Drawing.Point(15, 222);
+            this.FunImp.Name = "FunImp";
+            this.FunImp.Size = new System.Drawing.Size(124, 23);
+            this.FunImp.TabIndex = 7;
+            this.FunImp.Text = "Numeros Impares";
+            this.FunImp.UseVisualStyleBackColor = true;
+            this.FunImp.Click += new System.EventHandler(this.FunImpar_Click);
+            // 
+            // FunPar
+            // 
+            this.FunPar.Location = new System.Drawing.Point(12, 177);
+            this.FunPar.Name = "FunPar";
+            this.FunPar.Size = new System.Drawing.Size(124, 23);
+            this.FunPar.TabIndex = 7;
+            this.FunPar.Text = "Numeros Pares";
+            this.FunPar.UseVisualStyleBackColor = true;
+            this.FunPar.Click += new System.EventHandler(this.FunPar_Click);
+            // 
+            // FunPrim
+            // 
+            this.FunPrim.Location = new System.Drawing.Point(15, 306);
+            this.FunPrim.Name = "FunPrim";
+            this.FunPrim.Size = new System.Drawing.Size(124, 23);
+            this.FunPrim.TabIndex = 8;
+            this.FunPrim.Text = "Numeros Primos";
+            this.FunPrim.UseVisualStyleBackColor = true;
+            this.FunPrim.Click += new System.EventHandler(this.FunPrim_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(442, 51);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.FunPrim);
+            this.Controls.Add(this.FunPar);
+            this.Controls.Add(this.FunImp);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // Campos privados de la clase
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button FunImp;
+        private System.Windows.Forms.Button FunPar;
+        private System.Windows.Forms.Button FunPrim;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+
+        // Método para manejar el evento de clic en el gráfico chart1
+    }
+}
---
+++ Practico4/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practico4
+{
+    public partial class Form1 : Form
+    {
+        // Variables para controlar si el mensaje ya fue mostrado
+        private bool isTextBox1MessageShown = false;
+        private bool isTextBox2MessageShown = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Verificar que los campos no estén vacíos
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Ambos campos deben estar llenos.");
+                return;
+            }
+
+            // Verificar que los valores sean numéricos
+            if (!int.TryParse(textBox1.Text, out int startValue) || !int.TryParse(textBox2.Text, out int endValue))
+            {
+                MessageBox.Show("Ambos campos deben contener solo números.");
+                return;
+            }
+
+            // Limpiar el textBox3 y el gráfico antes de agregar los números
+            textBox3.Clear();
+            chart1.Series["Series1"].Points.Clear();
+
+            // Generar y mostrar los números en textBox3 y en el gráfico
+            for (int i = startValue; i <= endValue; i++)
+            {
+                textBox3.AppendText(i.ToString() + Environment.NewLine);
+                chart1.Series["Series1"].Points.AddXY(i, i); // Añade los números al gráfico
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Verificar que el campo solo contenga números
+            if (!int.TryParse(textBox1.Text, out _))
+            {
+                if (!isTextBox1MessageShown)
+                {
+                    MessageBox.Show("Este campo solo acepta números.");
+                    isTextBox1MessageShown = true; // Marcar que el mensaje ya fue mostrado
+                }
+                textBox1.Clear();
+            }
+            else
+            {
+                isTextBox1MessageShown = false; // Reiniciar la bandera si el valor es válido
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Verificar que el campo solo contenga números
+            if (!int.TryParse(textBox2.Text, out _))
+            {
+                if (!isTextBox2MessageShown)
+                {
+                    MessageBox.Show("Este campo solo acepta números.");
+                    isTextBox2MessageShown = true; // Marcar que el mensaje ya fue mostrado
+                }
+                textBox2.Clear();
+            }
+            else
+            {
+                isTextBox2MessageShown = false; // Reiniciar la bandera si el valor es válido
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            // Aquí podrías agregar lógica adicional para textBox3 si es necesario
+        }
+
+        // Función para mostrar solo los números pares
+        private void FunPar_Click(object sender, EventArgs e)
+        {
+            // Verificar que los campos no estén vacíos
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Ambos campos deben estar llenos.");
+                return;
+            }
+
+            // Verificar que los valores sean numéricos
+            if (!int.TryParse(textBox1.Text, out int startValue) || !int.TryParse(textBox2.Text, out int endValue))
+            {
+                MessageBox.Show("Ambos campos deben contener solo números.");
+                return;
+            }
+
+            // Limpiar el textBox3 y el gráfico antes de agregar los números
+            textBox3.Clear();
+            chart1.Series["Series1"].Points.Clear();
+
+            // Generar y mostrar solo los números pares en textBox3 y en el gráfico
+            for (int i = startValue; i <= endValue; i++)
+            {
+                if (i % 2 == 0) // Verifica si el número es par
+                {
+                    textBox3.AppendText(i.ToString() + Environment.NewLine);
+                    chart1.Series["Series1"].Points.AddXY(i, i); // Añade los números pares al gráfico
+                }
+            }
+        }
+
+        // Función para mostrar solo los números impares
+        private void FunImpar_Click(object sender, EventArgs e)
+        {
+            // Verificar que los campos no estén vacíos
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Ambos campos deben estar llenos.");
+                return;
+            }
+
+            // Verificar que los valores sean numéricos
+            if (!int.TryParse(textBox1.Text, out int startValue) || !int.TryParse(textBox2.Text, out int endValue))
+            {
+                MessageBox.Show("Ambos campos deben contener solo números.");
+                return;
+            }
+
+            // Limpiar el textBox3 y el gráfico antes de agregar los números
+            textBox3.Clear();
+            chart1.Series["Series1"].Points.Clear();
+
+            // Generar y mostrar solo los números impares en textBox3 y en el gráfico
+            for (int i = startValue; i <= endValue; i++)
+            {
+                if (i % 2 != 0) // Verifica si el número es impar
+                {
+                    textBox3.AppendText(i.ToString() + Environment.NewLine);
+                    chart1.Series["Series1"].Points.AddXY(i, i); // Añade los números impares al gráfico
+                }
+            }
+        }
+
+        // Función para mostrar solo los números primos
+        private void FunPrim_Click(object sender, EventArgs e)
+        {
+            // Verificar que los campos no estén vacíos
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Ambos campos deben estar llenos.");
+                return;
+            }
+
+            // Verificar que los valores sean numéricos
+            if (!int.TryParse(textBox1.Text, out int startValue) || !int.TryParse(textBox2.Text, out int endValue))
+            {
+                MessageBox.Show("Ambos campos deben contener solo números.");
+                return;
+            }
+
+            // Limpiar el textBox3 y el gráfico antes de agregar los números
+            textBox3.Clear();
+            chart1.Series["Series1"].Points.Clear();
+
+            // Generar y mostrar solo los números primos en textBox3 y en el gráfico
+            for (int i = startValue; i <= endValue; i++)
+            {
+                if (IsPrime(i)) // Verifica si el número es primo
+                {
+                    textBox3.AppendText(i.ToString() + Environment.NewLine);
+                    chart1.Series["Series1"].Points.AddXY(i, i); // Añade los números primos al gráfico
+                }
+            }
+        }
+
+        // Función para verificar si un número es primo
+        private bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            // Aquí puedes añadir la lógica que deseas ejecutar al hacer clic en el gráfico
+            chart1.Series["Series1"].Points.Clear(); // Limpia los puntos del gráfico
+            MessageBox.Show("Has hecho clic en el gráfico.", "Evento Click", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
